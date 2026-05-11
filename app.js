edcc3e40c375d9f42e9c294456d514c21c3afc56
diegoarv86 <diegoarv86@gmail.com>
2026-05-11 03:20:23 +0200

    fix path para vercel

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
 
 //creamos un middleware que diga cual es el directorio principal  de la pagina estatica
 //los middleware se ejecutan antes  del resto del contenido
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 //senfile para enviar archivo
 
 app.get('/', (req,res) => {
-    res.sendFile(`${__dirname}/public/index.html`);
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.get('/contacta',(req,res) =>{
-    res.sendFile(`${__dirname}/public/contacta.html`);
+    res.sendFile(path.join(__dirname, 'public', 'contacta.html'));
 });
 
 app.get('/localizacion',(req,res) =>{
-    res.sendFile(`${__dirname}/public/localizacion.html`);
+    res.sendFile(path.join(__dirname, 'public', 'localizacion.html'));
 });
 
 app.get('/*splat',(req,res) =>{
-    res.sendFile(`${__dirname}/public/404.html`);
+    res.sendFile(path.join(__dirname, 'public', '404.html'));
 });
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => { console.log(`htt´://localhost:${PORT}`) });
+app.listen(PORT, () => { console.log(`http://localhost:${PORT}`) });
