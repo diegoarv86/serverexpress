55a048570ca2907d26c937c036ad64d5c770cc55
diegoarv86 <diegoarv86@gmail.com>
2026-05-11 03:29:36 +0200

    fix vercel routes

--- app.js
+++ app.js
@@ -2,28 +2,19 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+const PUBLIC = path.join(__dirname, 'public');
+
 //creamos un middleware que diga cual es el directorio principal  de la pagina estatica
 //los middleware se ejecutan antes  del resto del contenido
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(PUBLIC));
 
 //senfile para enviar archivo
 
-app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-app.get('/contacta',(req,res) =>{
-    res.sendFile(path.join(__dirname, 'public', 'contacta.html'));
-});
-
-app.get('/localizacion',(req,res) =>{
-    res.sendFile(path.join(__dirname, 'public', 'localizacion.html'));
-});
-
-app.get('/*splat',(req,res) =>{
-    res.sendFile(path.join(__dirname, 'public', '404.html'));
-});
+app.get('/', (req, res) => res.sendFile(path.join(PUBLIC, 'index.html')));
+app.get('/contacta', (req, res) => res.sendFile(path.join(PUBLIC, 'contacta.html')));
+app.get('/localizacion', (req, res) => res.sendFile(path.join(PUBLIC, 'localizacion.html')));
+app.get('/*splat', (req, res) => res.sendFile(path.join(PUBLIC, '404.html')));
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => { console.log(`http://localhost:${PORT}`) });
+app.listen(PORT, () =>  console.log(`http://localhost:${PORT}`) );
